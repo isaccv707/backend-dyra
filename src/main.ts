8e474aabd2705ba2ff305911daa28325221a47e5
Isaac <ac7148126@gmail.com>
2026-02-09 14:02:52 -0600

    add cors origins

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = process.env.CORS_ORIGINS || 'http://localhost:4321';
+
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: '*',
-    
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      }
+      else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
   app.useGlobalPipes(
     new ValidationPipe({
