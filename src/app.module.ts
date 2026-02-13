9e1ec9ecac473ab1322545ede7f23f23e8e681fd
Isaac <ac7148126@gmail.com>
2026-02-12 21:40:03 -0600

    feat(blog-module): add new models in schema.prisma, Author, Post, ContentBlock

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { PrismaModule } from 'prisma/prisma/prisma.module';
 import { QuotationsModule } from './quotations/quotations.module';
 import { StudiesModule } from './studies/studies.module';
 import { BannersModule } from './banners/banners.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
   imports: [
@@ -13,7 +14,8 @@ import { BannersModule } from './banners/banners.module';
     PrismaModule,
     QuotationsModule,
     StudiesModule,
-    BannersModule
+    BannersModule,
+    BlogModule,
   ],
   controllers: [],
   providers: [],
