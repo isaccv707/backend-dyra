6c745b94c9a496bd5e2944010337f9db4a1f0ddb
Isaac <ac7148126@gmail.com>
2026-03-28 20:57:24 -0600

    feat(service-module): update service Model prisma

--- src/services/dto/create-service.dto.ts
+++ src/services/dto/create-service.dto.ts
@@ -44,6 +44,10 @@ export class CreateServiceDto {
     @IsString()
     description?: string;
 
+    @IsOptional()
+    @IsString()
+    iconUrl?: string;
+
     @IsString()
     imageUrl: string;
 
