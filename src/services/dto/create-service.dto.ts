220a32e1cdba861f2a35baf16f49b6a63d9b0866
Isaac <ac7148126@gmail.com>
2026-07-02 12:14:28 -0600

    update DTO Create Service

--- src/services/dto/create-service.dto.ts
+++ src/services/dto/create-service.dto.ts
@@ -1,73 +1,81 @@
-import { Type } from "class-transformer";
-import { IsArray, IsBoolean, IsNotEmpty, IsOptional, IsString, MinLength, ValidateNested } from "class-validator";
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
 
 export class BenefitDto {
-    @IsString()
-    @IsNotEmpty()
-    @MinLength(3)
-    title: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  title!: string;
 
-    @IsString()
-    @IsOptional()
-    description?: string;
+  @IsString()
+  @IsOptional()
+  description?: string;
 
-    @IsString()
-    @IsOptional()
-    icon?: string;
+  @IsString()
+  @IsOptional()
+  icon?: string;
 }
 
 export class ServiceDetailDto {
-    @IsNotEmpty()
-    @IsString()
-    title: string;
+  @IsNotEmpty()
+  @IsString()
+  title!: string;
 
-    @IsOptional()
-    @IsString()
-    description?: string;
+  @IsOptional()
+  @IsString()
+  description?: string;
 
-    @IsOptional()
-    @IsString()
-    image?: string;
+  @IsOptional()
+  @IsString()
+  image?: string;
 }
 
 export class CreateServiceDto {
-    @IsString()
-    @IsNotEmpty()
-    name: string;
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
 
-    @IsString()
-    // @IsNotEmpty()
-    @IsOptional()
-    slug?: string;
+  @IsString()
+  // @IsNotEmpty()
+  @IsOptional()
+  slug?: string;
 
-    @IsOptional()
-    @IsString()
-    description?: string;
+  @IsOptional()
+  @IsString()
+  description?: string;
 
-    @IsOptional()
-    @IsString()
-    icon?: string;
+  @IsOptional()
+  @IsString()
+  icon?: string;
 
-    @IsString()
-    imageUrl: string;
+  @IsString()
+  imageUrl!: string;
 
-    @IsOptional()
-    @IsOptional()
-    mobileImageUrl?: string;
+  @IsOptional()
+  @IsOptional()
+  mobileImageUrl?: string;
 
-    @IsOptional()
-    @IsBoolean()
-    isActive?: boolean;
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
 
-    @IsArray()
-    @ValidateNested({ each: true })
-    @Type(() => BenefitDto)
-    @IsOptional()
-    benefits?: BenefitDto[];
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => BenefitDto)
+  @IsOptional()
+  benefits?: BenefitDto[];
 
-    @IsArray()
-    @ValidateNested({ each: true })
-    @Type(() => ServiceDetailDto)
-    @IsOptional()
-    details?: ServiceDetailDto[];
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ServiceDetailDto)
+  @IsOptional()
+  details?: ServiceDetailDto[];
 }
